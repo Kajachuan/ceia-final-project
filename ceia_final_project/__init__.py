2e2f0679fcdccd68adb7dd7077cac4285c4d8a6e
Kevin Cajachuán <kevincajachuan@hotmail.com>
2025-04-02 14:09:50 -0300

    fix version in init

--- ceia_final_project/__init__.py
+++ ceia_final_project/__init__.py
@@ -2,4 +2,11 @@ from . import datasets
 from . import modules
 from . import transforms
 
-__all__ = ["datasets", "modules", "transforms"]
+from .__version__ import __version__
+
+__all__ = [
+    "datasets", 
+    "modules", 
+    "transforms", 
+    "__version__",
+]
